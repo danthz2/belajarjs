79aff9cd2e92a37d40c0138e3a7c43a09a63a397
danthz2 <faqihsidj@gmail.com>
2020-09-29 23:41:59 +0700

    Belajar Looping for, foreach, map

--- script.js
+++ script.js
@@ -8,63 +8,63 @@ window.addEventListener("scroll", function () {
 
 var mahasiswa = [
   {
-    "nama": "Ahmad Faqih",
+    "name": "Ahmad Faqih",
     "nim": 1603015117,
-    "notelp": 85813834424,
+    "notelp": 6285813834424,
     "email": "faqihsidj@gmail.com"
   },
   {
-    "nama": "Muhammad Irva",
+    "name": "Muhammad Irva",
     "nim": 1603015118,
-    "notelp": 85814834425,
+    "notelp": 6285814834425,
     "email": "irva.mhd@gmail.com"
   },
   {
-    "nama": "Faishal Mustofa",
+    "name": "Faishal Mustofa",
     "nim": 1603015119,
-    "notelp": 85815234424,
+    "notelp": 6285815234424,
     "email": "ichals@gmail.com"
   },
   {
-    "nama": "Bayu Aji",
+    "name": "Bayu Aji",
     "nim": 1603015120,
-    "notelp": 85721334424,
+    "notelp": 6285721334424,
     "email": "bayuaji@gmail.com"
   },
   {
-    "nama": "Muhammad Rafly",
+    "name": "Muhammad Rafly",
     "nim": 1603015121,
-    "notelp": 85752334424,
+    "notelp": 6285752334424,
     "email": "m.raflyr@gmail.com"
   },
   {
-    "nama": "Diki Pratama",
+    "name": "Diki Pratama",
     "nim": 1603015122,
-    "notelp": 85813834424,
+    "notelp": 6285813834424,
     "email": "dikidpr@gmail.com"
   },
   {
-    "nama": "Raka Rizky",
+    "name": "Raka Rizky",
     "nim": 1603015123,
-    "notelp": 85815312424,
+    "notelp": 6285815312424,
     "email": "rakarzky@gmail.com"
   },
   {
-    "nama": "Dede Hidayat",
+    "name": "Dede Hidayat",
     "nim": 1603015124,
-    "notelp": 85816598424,
+    "notelp": 6285816598424,
     "email": "dedehdyt@gmail.com"
   },
   {
-    "nama": "Muhammad Alfian",
+    "name": "Muhammad Alfian",
     "nim": 1603015125,
-    "notelp": 8581923224,
+    "notelp": 628581923224,
     "email": "m.alfian@gmail.com"
   },
   {
-    "nama": "Candra Ageng",
+    "name": "Candra Ageng",
     "nim": 1603015126,
-    "notelp": 85898643224,
+    "notelp": 6285898643224,
     "email": "ccandra.12@gmail.com"
   }
 ];
@@ -87,24 +87,33 @@ var mahasiswa = [
 // })
 // var namaMahasiswa = mahasiswa.map(val => val.nama);
 
-(function () {
+
+// Fungsi Looping dengan For
+function forFunc() {
   var text = "";
   for (var i = 0; i < mahasiswa.length; i++) {
     text = mahasiswa[i].nama + "\n";
     console.log(text);
   }
-})();
+};
 
-(function () {
+//Fungsi Looping dengan forEach
+function fEachFunc() {
   var text = "";
   mahasiswa.forEach(function (e) {
     text = e.nim + "\n";
     console.log(text);
   })
-})();
+};
 
-(function () {
-  var text = "";
+//Fungsi Looping dengan map
+function mapFunc() {
   var dataMhs = mahasiswa.map(val => val.email);
   console.log(dataMhs.join("\n"));
+};
+
+
+// self invok Function
+(function () {
+  fEachFunc();
 })();
